15d15406ea73dc8b9a57fd1154f08348e86bdb61
langleu <lars@langl.eu>
2020-06-27 21:19:07 +0200

    chore(grakn): update schema and upgrade to 1.8.0

--- src/database/grakn/templates/deployment.js
+++ src/database/grakn/templates/deployment.js
@@ -1,5 +1,8 @@
 module.exports = (deployment) => {
   const { rid, rtype, rawUrl, name, executable, version } = deployment;
+  const schemaVersion = 1;
+  const updated = new Date().toISOString().split('T')[0];
+  const created = new Date().toISOString().split('T')[0];
 
   let graqlInsertQuery = `insert $deployment isa deployment, has rid "${rid}"`;
   
@@ -8,6 +11,9 @@ module.exports = (deployment) => {
   graqlInsertQuery += `, has rawUrl "${rawUrl}"`;
   graqlInsertQuery += `, has executable "${executable}"`;
   graqlInsertQuery += `, has version "${version}"`;
+  graqlInsertQuery += `, has schemaVersion "${schemaVersion}"`;
+  graqlInsertQuery += `, has updated "${updated}"`;
+  graqlInsertQuery += `, has created "${created}"`;
 
   graqlInsertQuery += ";";
   return graqlInsertQuery;
