016ac696c8e601dffe5fdf2185f89727e730388d
langleu <lars@langl.eu>
2020-06-28 01:13:46 +0200

    chore(orchestrate): start simple orchestration to nodes

--- src/api/v1/orchestrate.js
+++ src/api/v1/orchestrate.js
@@ -1,31 +1,49 @@
 const Router = require('@koa/router');
 const availableCrawlers = ['GitHubAPI']; // TODO: add to config
+const maxFileSize = 1000000; // 10mb in bytes, we assume for now that the max file size will be 10 mb
 
 module.exports = (io) => {
-  const router = new Router({prefix: '/v1/orchestrate'});
+  const router = new Router({
+    prefix: '/v1/orchestrate'
+  });
 
-router.get('/', async (ctx, next) => {
-  ctx.body = {
-    availableCrawlers,
-  };
-});
+  router.get('/', async (ctx, next) => {
+    ctx.body = {
+      availableCrawlers,
+    };
+  });
 
-router.post('/', async (ctx, next) => {
-  const data = ctx.request.body;
-  const type = data.crawler;
+  router.post('/', async (ctx, next) => {
+    const data = ctx.request.body;
+    const type = data.crawler || data.type;
+    const term = data.term;
 
-  if (availableCrawlers.includes(type)) {
-    ctx.body = true;
+    if (availableCrawlers.includes(type)) {
+      ctx.body = true;
+      let nodes = Object.keys(io.sockets.sockets).length;
 
-    // TODO: from here on divide depending on amount of clients and send pages x to y.
+      if (nodes == 0) {
+        // TODO: possibly run the execution on the master?
+        ctx.throw(500, 'master currently can`t handle any requests!');
+      } else if (nodes > 0) {
+        // orchestrate to nodes
 
-    console.log(Object.keys(io.sockets.sockets));
+        let fragment = Math.round(maxFileSize / nodes);
 
-  } else {
-    ctx.throw(400, 'crawler not implemented!');
-  }
-});
+        Object.keys(io.sockets.sockets).forEach( (node, index) => {
+          let request = {
+            ending: fragment * (index + 1),
+            beginning: fragment * index,
+            term,
+            crawler: type,
+          }
+          io.sockets.sockets[node].emit('crawl', `${JSON.stringify(request)}`);
+        });
+      }
+    } else {
+      ctx.throw(400, 'crawler not implemented!');
+    }
+  });
 
-return router;
+  return router;
 }
-
--- src/messaging/client/index.js
+++ src/messaging/client/index.js
@@ -1,5 +1,7 @@
 const socket = require('socket.io-client')('http://localhost:8000');
 const axios = require('axios').default;
+const gCrawler = require('./../../crawler/GenericCrawler');
+const gProcessor = require('./../../processor/GenericProcessor');
 
 socket.on('connect', () => {
   console.log('connected');
@@ -8,10 +10,24 @@ socket.on('event', (data) => {
   console.log(data);
 });
 
-socket.on('crawl', (data) => {
+socket.on('crawl', async (data) => {
+  let pData = JSON.parse(data);
+  console.log(`crawl: ${JSON.stringify(pData)}`);
+
+  let crawler = new gCrawler(pData.crawler);
+  let processor = new gProcessor(pData.crawler);
+  let res = await crawler.crawl({
+    term: pData.term,
+    size: 105
+  });
+
+  let res2 = await processor.batchProcess(res);
+  console.log(res2);
+
   // TODO: call up /crawl with axios and page 1,2,3... sent result to processing or return to master.
 });
 
 socket.on('disconnect', () => {
-  
+  // save progress
+  // either on process SIGINT or exit  
 });
